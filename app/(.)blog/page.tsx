9e1058bfb2af8e2068ce0e9865e772bce0aad646
Nikhil Sharma <nikhilsharmalku@gmail.com>
2024-07-14 23:25:42 +0530

    14.07.2024.c1

---
+++ app/(.)blog/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const blog2 = () => {
+  return (
+    <div>
+      <h1>
+        this is blog
+      </h1>
+    </div>
+  )
+}
+
+export default blog2
--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,25 @@ const page = () => {
         Coding has always been more than just a skill for me; its a creative outlet and a way to solve real-world problems. Im excited to continue my journey in technology, learn new things, and contribute to innovative projects.
         </p>
       </div>
+      <hr></hr>
+      <div className='education-card-container'>
+        <ul className='education'>
+          <li className='education-heading'><h2>College:</h2></li>
+          <ul className='education-data'>
+            <li><h3>BTech, Mechanical Engineering</h3></li>
+            <li><h4>VNIT, Nagpur</h4></li>
+            <li><h4>2023 - 2027</h4></li>
+          </ul>
+        </ul>
+        <ul className='education'>
+          <li className='education-heading'><h2>School:</h2></li>
+          <ul className='education-data'>
+            <li><h3>CBSE, Secondary (XII), Science</h3></li>
+            <li><h4>Naini Valley School</h4></li>
+            <li><h4>2018-2022</h4></li>
+          </ul>
+        </ul>
+      </div>
     </>
  )
 }
