0cf79667821304a10abf7cc5d5dea67a473d8ceb
Nikhil Sharma <nikhilsharmalku@gmail.com>
2024-10-27 23:17:47 +0530

    27th oct

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -29,6 +29,22 @@ const Educard: React.FC<EduCardProps> = ({institute, duration, course}) => {
     )
 }
 
+type codingProfileProps = {
+  sitename: string;
+  imgurl: string;
+};
+
+const CodingProfile: React.FC<codingProfileProps> = ({sitename, imgurl}) => {
+  return(
+        <div className="coding-profile-card">
+          <div className="coding-profile-img">
+            <Image src={imgurl} width={50} height={50} alt={sitename}/>
+          </div>
+          <h3>{sitename}</h3>
+        </div>
+  )
+}
+
 const Resume = () => {
   return (
     <>
@@ -51,6 +67,19 @@ const Resume = () => {
       </div>
       <Educard institute={"ECom Business Talks, Deepak Adhav - Video Editor"} duration={"AUG-OCT 2023"} course={"handling YouTube channel of Japan based NRI"}/>
       <Educard institute={"Bankpreneur, Aaradhya Mudghal - Video Editor"} duration={"JULY 2023"} course={"IIT BHU Alumni social handle"}/>
+      <div className="edu-heading">
+          <div>
+            <Image src="/exp.png" alt="experience" width={35} height={20} className='invert'></Image>
+          </div>
+          <h1>Coding Profiles</h1>
+      </div>
+      <div className="coding-profiles">
+        <CodingProfile sitename={"Hacker Rank"} imgurl={'/phone.png'}/>
+        <CodingProfile sitename={"LeetCode"} imgurl={'/phone.png'}/>
+        <CodingProfile sitename={"Code Chef"} imgurl={'/phone.png'}/>
+        <CodingProfile sitename={"Code Forces"} imgurl={'/phone.png'}/>
+        <CodingProfile sitename={"Hacker Earth"} imgurl={'/phone.png'}/>
+      </div>
     </div>
   </>
   )
